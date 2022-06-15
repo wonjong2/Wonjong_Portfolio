b0e5770f1d34742996efb2ae8daeccd8410596db
Wonjong Park <wonjong2@gmail.com>
2022-06-14 21:54:30 -0700

    Updates my Work with the latest HW and Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ var workEl = document.getElementById("work-list");
 
 // Array for gitHub URLs
 const gitHubUrl = {
-    "first-project": "https://github.com/levisgaragegroupinc/translation_app",
-    "second-project": "https://github.com/wonjong2/Week6_Weather_Dashboard",
-    "third-project": "https://github.com/wonjong2/Week5_Work_Day_Scheduler",
-    "fourth-project": "https://www.google.com/",
-    "fifth-project": "https://www.google.com/"
+    "first-project": "https://github.com/wonjong2/Bens_Diner",
+    "second-project": "https://github.com/levisgaragegroupinc/translation_app",
+    "third-project": "https://github.com/wonjong2/Tech_Blog_MVC",
+    "fourth-project": "https://github.com/wonjong2/Week6_Weather_Dashboard",
+    "fifth-project": "https://github.com/wonjong2/Week5_Work_Day_Scheduler"
 }
 
 //  Added event listener 
@@ -20,7 +20,7 @@ function handleClickEventOnWork(event) {
     var url;
 
     // If a user click "[Go To GitHub  ]", then link to GitHub page
-    if(eventEl.nodeName === "P") {
+    if (eventEl.nodeName === "P") {
         url = gitHubUrl[event.target.id];
     }
     // Else link to the deployed web page
